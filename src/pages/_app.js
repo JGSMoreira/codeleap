623c0406402ac2d26f23f8fe209934e241b20b84
João Guilherme <jgmoreir2@gmail.com>
2023-06-27 20:30:38 -0400

    using inter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,27 @@
 import { Provider } from "react-redux";
 import store from "../redux/store";
-import "./globals.css";
 import Layout from "@/components/layout";
 import { LoadingProvider } from "@/components/modal/loading/loadingProvider";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <LoadingProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </LoadingProvider>
-    </Provider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Provider store={store}>
+        <LoadingProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </LoadingProvider>
+      </Provider>
+    </>
   );
 }
